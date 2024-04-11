ede13caeb8e7fb04cd880df0357dc2028ef0b0dd
Mikov Denis <denis.mikov.06@gmail.com>
2024-04-11 22:06:11 +0300

    feat(player): implemented dash

--- IGJ2024/Assets/Scripts/Combat/Player/PlayerMovement.cs
+++ IGJ2024/Assets/Scripts/Combat/Player/PlayerMovement.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField, Min(0)] private float moveSpeed = 1f;
+    
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashDuration;
+    [SerializeField] private float jumpStrength;
+
+    private float _dashTimeLeft;
+    private bool IsDashing => _dashTimeLeft > 0;
+
     private Rigidbody2D _rigidbody;
+    private float _horizontalInput;
 
     private void Start()
     {
@@ -13,7 +22,31 @@ public class PlayerMovement : MonoBehaviour
 
     public void FixedUpdate()
     {
-        var horizontal = Input.GetAxis("Horizontal");
-        _rigidbody.MovePosition(_rigidbody.position + horizontal * moveSpeed * Time.fixedDeltaTime * Vector2.right);
+        if (IsDashing)
+        {
+            _rigidbody.position += _horizontalInput * dashSpeed * Time.fixedDeltaTime * Vector2.right;
+            _dashTimeLeft -= Time.fixedDeltaTime;
+        }
+        else
+        {
+            _horizontalInput = Input.GetAxis("Horizontal");
+            _rigidbody.position += _horizontalInput * moveSpeed * Time.fixedDeltaTime * Vector2.right;
+        }
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            PerformDash();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            PerformJump();
+    }
+
+    private void PerformJump()
+    {
+        _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jumpStrength);
+    }
+
+    private void PerformDash() => _dashTimeLeft = dashDuration;
 }
