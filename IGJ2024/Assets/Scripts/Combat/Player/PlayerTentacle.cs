632f8eebc0e756ac8280cac70f5a7b74a854586d
Mikov Denis <denis.mikov.06@gmail.com>
2024-04-13 14:18:29 +0300

    feat(player): implemented tentacle speed boost

--- IGJ2024/Assets/Scripts/Combat/Player/PlayerTentacle.cs
+++ IGJ2024/Assets/Scripts/Combat/Player/PlayerTentacle.cs
@@ -1,10 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PlayerTentacle : MonoBehaviour
 {
     [SerializeField] private float hookRange;
     [SerializeField] private LayerMask hookSurface;
-    [SerializeField] private float hookSpeed;
+    [SerializeField] private LayerMask possibleHookTargets;
+    [SerializeField] private float hookSpeedMultiplier;
+    [SerializeField] private float hookGrowthSpeed;
+    [SerializeField] private AnimationCurve hookSpeed;
+    [SerializeField] private float hookBoostTime;
+
+    private float _hookMountElapsedTime;
     private LineRenderer _line;
     private Rigidbody2D _rigidbody;
     private bool _isHooking;
@@ -21,41 +28,81 @@ public class PlayerTentacle : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, mousePosition - _rigidbody.position, hookRange, hookSurface.value);
-            if (hit.collider != null)
+            RaycastHit2D hit = Physics2D.Raycast(_rigidbody.position, mousePosition - _rigidbody.position, hookRange, possibleHookTargets.value);
+
+            bool hasMissed = hit.collider == null;
+            bool isHitSuccessful = false;
+            
+            // By default, player has missed.
+            // The tentacle will try to reach the point in that direction by the tentacle range
+            _hookTarget = mousePosition;
+
+            // In case if player has hit something
+            if (!hasMissed)
             {
-                _isHooking = true;
-                _hookTarget = hit.point;
+                // In case if the player hit the pipes - he successfully sucks on it
+                isHitSuccessful = Physics2D.OverlapCircle(hit.point, .1f, hookSurface.value);
+                _hookTarget = hit.point; // the point will be overwritten in any case
             }
 
-            DrawHookLine();
+            EnableHook();
+            StartCoroutine(LaunchTentacle(isHitSuccessful));
         }
 
         if (Input.GetMouseButtonUp(0))
         {
             _isHooking = false;
             ClearHookLine();
+            _hookMountElapsedTime = 0f;
         }
+    }
+
+    private IEnumerator LaunchTentacle(bool hasHit)
+    {
+        var direction = (_rigidbody.position - _hookTarget).normalized;
+
+        var currentPoint = _rigidbody.position;
+        var isGettingCloser = true;
+        var previousDistance = Vector2.Distance(currentPoint, _hookTarget);
+
+        while (previousDistance > .1f && isGettingCloser)
+        {
+            currentPoint -= direction * hookGrowthSpeed;
+            isGettingCloser = Vector2.Distance(currentPoint, _hookTarget) < previousDistance;
+            previousDistance = Vector2.Distance(currentPoint, _hookTarget); 
+            UpdateHookLine(currentPoint);
+            yield return null;
+        }
+        _isHooking = hasHit;
     }
 
     private void FixedUpdate()
     {
         if (_isHooking)
+            RideHook();
+    }
+
+    private void RideHook()
+    {
+        float currentSpeed = EvaluateSpeed();
+
+        Vector2 hookDirection = (_hookTarget - (Vector2)transform.position).normalized;
+        _rigidbody.velocity = hookDirection * currentSpeed;
+        UpdateHookLine(_hookTarget);
+
+        float EvaluateSpeed()
         {
-            Vector2 hookDirection = (_hookTarget - (Vector2)transform.position).normalized;
-            _rigidbody.velocity = hookDirection * hookSpeed;
-            DrawHookLine();
+            float clamp = Mathf.Clamp01(_hookMountElapsedTime / hookBoostTime);
+            float currentSpeed = hookSpeed.Evaluate(clamp) * hookSpeedMultiplier;
+            _hookMountElapsedTime += Time.fixedDeltaTime;
+            return currentSpeed;
         }
     }
 
-    private void DrawHookLine()
-    {
-        _line.enabled = true;
-        _line.SetPositions(new Vector3[] { transform.position + Vector3.up * .5f, _hookTarget });
-    }
+    private void UpdateHookLine(Vector2 endpoint) 
+        => _line.SetPositions(new Vector3[] { transform.position + Vector3.up * .5f, endpoint });
 
-    private void ClearHookLine()
-    {
-        _line.enabled = false;
-    }
+    private void EnableHook() => _line.enabled = true;
+
+    private void ClearHookLine() => _line.enabled = false;
 }
