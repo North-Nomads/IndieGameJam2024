4af1d6f81c07c313c814d98f833195b24f6c7dba
Mikov Denis <denis.mikov.06@gmail.com>
2024-04-24 10:48:11 +0300

    wip

--- IGJ2024/Assets/Scripts/Combat/Player/PlayerTentacle.cs
+++ IGJ2024/Assets/Scripts/Combat/Player/PlayerTentacle.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections;
+using System.Runtime.CompilerServices;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerVFX))]
@@ -12,12 +14,8 @@ public class PlayerTentacle : MonoBehaviour
     [SerializeField] private LayerMask hookSurface;
     [SerializeField] private LayerMask possibleHookTargets;
     [SerializeField] private Transform hookPivot;
-
-    [Header("Speed")]
-    [SerializeField] private AnimationCurve hookSpeed;
-    [SerializeField] private float hookSpeedMultiplier;
-    [SerializeField] private float hookGrowthSpeed;
-    [SerializeField] private float hookBoostTime;
+    [SerializeField] private float playerRidingHookSpeed;
+    [SerializeField] private float hookSpeed;
 
     // GameObject components
     private PlayerMovement _playerMovement;
@@ -29,15 +27,11 @@ public class PlayerTentacle : MonoBehaviour
     
     // Fields
     private Coroutine _launchingTentacleProcess;
-    private float _hookMountElapsedTime;
-    private Vector2 _hookOrigin;
     private Vector2 _hookTarget;
     private bool _isHooking;
     private float _currentSpeed;
-    private float _windSpeed;
-
-    private bool HasReachedHookTarget => Vector2.Distance(_hookTarget, _hookOrigin) < 1f;
-
+    
+    private Vector2 HookOrigin => new (hookPivot.position.x, hookPivot.position.y);
 
     private void Start()
     {
@@ -48,9 +42,6 @@ public class PlayerTentacle : MonoBehaviour
         _animator = GetComponent<Animator>();
         _playerMovement = GetComponent<PlayerMovement>();
         _playerMovement.OnPlayerLanded += HandlePlayerLanding;
-
-        _windSpeed = hookSpeed.Evaluate(1) * .05f * Time.fixedDeltaTime;
-
         LevelObserver.OnLevelPaused += HandleLevelPause;
     }
 
@@ -61,33 +52,79 @@ public class PlayerTentacle : MonoBehaviour
         _isHooking = false;
     }
 
-    private void HandlePlayerLanding(object sender, EventArgs e)
-    {
-        _currentSpeed = 0f;
-    }
+    private void HandlePlayerLanding(object sender, EventArgs e) => _currentSpeed = 0f;
 
     private void Update()
     {
         if (LevelObserver.IsLevelPaused)
             return;
 
-        
-        _hookOrigin = new (hookPivot.position.x, hookPivot.position.y);
-
         if (Input.GetMouseButtonDown(0))
-        {
             LaunchTentacle();
-            _animator.SetBool("IsHooking", true);
-        }
 
         if (Input.GetMouseButtonUp(0))
-        {
             ReleaseTentacle();
-            _animator.SetBool("IsHooking", false);
-        }
     }
 
     private void ReleaseTentacle()
+    {
+        HideTentacle();
+        _animator.SetBool("IsHooking", false);
+    }
+
+    private void LaunchTentacle()
+    {
+        // Turn towards the hook 
+        Vector2 mousePosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        _playerMovement.TurnTowards(mousePosition.x > HookOrigin.x);
+
+        // Play launch animation
+        // Suck into hookable object
+        // Start moving towards the object
+        HandleTentacleLaunchAnimatiomTowards(mousePosition);
+    }
+
+    private void HandleTentacleLaunchAnimatiomTowards(Vector2 mousePosition)
+    {
+        StartCoroutine(PlayTentacleLaunchAnimation());
+        
+        IEnumerator PlayTentacleLaunchAnimation()
+        {
+            var direction = (mousePosition - HookOrigin).normalized;
+            var maxTentacleDistance = direction * hookRange;
+
+            var currentPoint = HookOrigin;
+            var isGettingCloser = true;
+            var previousDistance = Vector2.Distance(currentPoint, maxTentacleDistance);
+
+            while (previousDistance > .1f && isGettingCloser)
+            {
+                currentPoint -= direction * hookSpeed;
+                isGettingCloser = Vector2.Distance(currentPoint, maxTentacleDistance) < previousDistance;
+                previousDistance = Vector2.Distance(currentPoint, maxTentacleDistance);
+                UpdateHookLine(currentPoint);
+
+                if (IsTentacleTipSucked(currentPoint))
+                    HandleTentacleSuccessfullySuckedInto(currentPoint);
+
+                yield return null;
+            }
+
+            HideTentacle();
+        }
+    }
+
+    private void HandleTentacleSuccessfullySuckedInto(Vector2 targetPoint)
+    {
+        _playerVFX.SpawnHitVFX(_hookTarget);
+        _playerVFX.AnimateCameraZoom(true);
+        _animator.SetBool("IsHooking", true);
+        _hookTarget = targetPoint;
+    }
+
+    private bool IsTentacleTipSucked(Vector2 tentacleTipPoint) => Physics2D.OverlapCircle(tentacleTipPoint, .1f, hookSurface.value);
+
+    private void HideTentacle()
     {
         if (_launchingTentacleProcess is not null)
             StopCoroutine(_launchingTentacleProcess);
@@ -99,63 +136,6 @@ public class PlayerTentacle : MonoBehaviour
         // Reset variables for the next launch
         _isHooking = false;
         ClearHookLine();
-        _hookMountElapsedTime = 0f;
-    }
-
-    private void LaunchTentacle()
-    {
-        Vector2 mousePosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        RaycastHit2D hit = Physics2D.Raycast(_hookOrigin, mousePosition - _hookOrigin, hookRange, possibleHookTargets.value);
-
-        _playerMovement.TurnTowards(mousePosition.x > _hookOrigin.x);
-
-        ProcessLaunchResult(mousePosition, hit, out bool wasClickMiss, out bool wasHitSuccessful);
-
-        EnableHook();
-        _launchingTentacleProcess = StartCoroutine(LaunchTentacle(wasHitSuccessful, !wasClickMiss));
-
-        void ProcessLaunchResult(Vector2 mousePosition, RaycastHit2D hit, out bool hasMissed, out bool isHitSuccessful)
-        {
-            hasMissed = hit.collider == null;
-            isHitSuccessful = false;
-
-            // By default, player has missed.
-            // The tentacle will try to reach the point in that direction by the tentacle range
-            _hookTarget = _rigidbody.position + (mousePosition - _rigidbody.position).normalized * hookRange;
-
-            // In case if player has hit something
-            if (!hasMissed)
-            {
-                // In case if the player hit the pipes - he successfully sucks on it
-                isHitSuccessful = Physics2D.OverlapCircle(hit.point, .1f, hookSurface.value);
-                _hookTarget = hit.point; // the point will be overwritten in any case
-            }
-        }
-    }
-
-    private IEnumerator LaunchTentacle(bool hasHit, bool shouldSpawnVFX)
-    {
-        var direction = (_hookOrigin - _hookTarget).normalized;
-
-        var currentPoint = _hookOrigin;
-        var isGettingCloser = true;
-        var previousDistance = Vector2.Distance(currentPoint, _hookTarget);
-
-        while (previousDistance > .1f && isGettingCloser)
-        {
-            currentPoint -= direction * hookGrowthSpeed;
-            isGettingCloser = Vector2.Distance(currentPoint, _hookTarget) < previousDistance;
-            previousDistance = Vector2.Distance(currentPoint, _hookTarget); 
-            UpdateHookLine(currentPoint);
-            yield return null;
-        }
-        
-        _isHooking = hasHit;
-        if (shouldSpawnVFX)
-            _playerVFX.SpawnHitVFX(_hookTarget);
-
-        if (_isHooking)
-            _playerVFX.AnimateCameraZoom(true);
     }
 
     private void FixedUpdate()
@@ -169,33 +149,14 @@ public class PlayerTentacle : MonoBehaviour
 
     private void RideHook()
     {
-        _currentSpeed = EvaluateSpeed();
+        _currentSpeed = playerRidingHookSpeed;
 
-        Vector2 hookDirection = (_hookTarget - _hookOrigin).normalized;
+        Vector2 hookDirection = (_hookTarget - HookOrigin).normalized;
         _rigidbody.velocity = hookDirection * _currentSpeed;
         UpdateHookLine(_hookTarget);
-
-        float EvaluateSpeed()
-        {
-            // During the boost time -> increase the speed
-            // Once the boosting time is over - maintain end point speed
-
-            _hookMountElapsedTime += Time.fixedDeltaTime;
-            float clamp = Mathf.Clamp01(_hookMountElapsedTime / hookBoostTime);
-            float newSpeed;
-            if (clamp < 1)
-                newSpeed = _currentSpeed + hookSpeed.Evaluate(clamp) * hookSpeedMultiplier;
-            else if (HasReachedHookTarget)
-                newSpeed = 0f;
-            else
-                newSpeed = _currentSpeed - _windSpeed;
-
-            return Mathf.Clamp(newSpeed, 0, MaxSpeed);
-        }
     }
 
-    private void UpdateHookLine(Vector2 endpoint) 
-        => _line.SetPositions(new Vector3[] { _hookOrigin, endpoint });
+    private void UpdateHookLine(Vector2 endpoint) => _line.SetPositions(new Vector3[] { HookOrigin, endpoint });
 
     private void EnableHook() => _line.enabled = true;
 
